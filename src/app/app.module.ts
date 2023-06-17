c9b210c29a875f76fcfcc4ee918b24d4f38d24b7
Hesham Orabi <hshawky1050@yahoo.com>
2023-06-17 19:38:26 +0300

    Third-Day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,19 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ShopComponent } from './shop/shop.component';
-import { SharedComponent } from './shared/shared.component';
-import { PagerComponent } from './shared/components/pager/pager.component';
-import { PagingHeaderComponent } from './shared/components/paging-header/paging-header.component';
+import { CoreModule } from './core/core.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    ShopComponent,
-    SharedComponent,
-    PagerComponent,
-    PagingHeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
